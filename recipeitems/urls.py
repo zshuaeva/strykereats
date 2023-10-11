8be7537ef952eefb8ed697c60bcf680408fbd24a
Joshua <jpe7619@gmail.com>
2023-10-11 15:12:00 -0700

    update views

--- recipeitems/urls.py
+++ recipeitems/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from recipeitems.views import RecipeListView
 
 urlpatterns = [
-    path("recipes/", RecipeListView.as_view(), name="recipe-list"),
+    path("recipes/<int:recipe_id>", RecipeListView.as_view(), name="recipe_title"),
 ]
--- recipeitems/views.py
+++ recipeitems/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from .models import Recipe
 
 
@@ -8,5 +8,8 @@ class RecipeListView(ListView):
     template_name = "recipe/recipe_list.html"
     context_object_name = "recipes"
 
-    def get_queryset(self):
-        return Recipe.objects.all()
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = "recipe/recipe_detail.html"
+    context_object_name = "recipe"
